d9633a3c4f2f9ceb3fdeaa9a3e4686b751df31a3
Steven4Hooisma <s.hooisma@cleverzebra.nl>
2025-11-16 13:56:10 +0100

    Support multiple api versions

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -8,8 +8,16 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
-        api: __DIR__.'/../routes/api_v1.php',
         health: '/up',
+        then: function () {
+            foreach (\App\Enums\ApiVersion::cases() as $key => $value) {
+                $version = $value->value;
+                Route::prefix("api/{$version}")
+                    ->name("api.{$version}.")
+                    ->middleware('api')
+                    ->group(base_path("routes/api_{$version}.php"));
+            }
+        }
     )
     ->withMiddleware(function (Middleware $middleware) {
         //
